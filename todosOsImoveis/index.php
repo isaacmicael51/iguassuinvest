c9fd24f76cb344e8206bbb1bb029b193ffc230e4
Isaac Spagnol <isaacmicael51@gmail.com>
2021-12-04 17:24:11 -0300

    menu paginação

--- todosOsImoveis/index.php
+++ todosOsImoveis/index.php
@@ -57,7 +57,19 @@ require '../backend/imoveisDisponiveis/index.php';
         echo "";
         echo "</div>";
 
-?>
+
+                $quantidade = $obj_merged->quantidade;
+                $totalDePaginas = $quantidade / 20;    
+                $nPage =  1;
+            ?>
+        <div class="col-12 ">
+            <ul class="pagination ">
+            <?php while($nPage < $totalDePaginas){
+                    echo "<li class='page-item'><a class='page-link' href='#'>" . (int)  $nPage . "</a></li>";
+                    $nPage ++;
+                }?>
+            </ul>
+        </div>
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
